1b6f6b276d4d9e012dbb32a600c1fe0635e9f646
Dineshwar-2501 <mahesg12war456@gmail.com>
2025-03-12 12:24:30 +0530

    db

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // Load environment variables
 
 // Ensure DATABASE_URL is correctly set in Railway
-const pool = mysql.createPool(process.env.DATABASE_URL || {
+const pool = mysql.createPool(process.env.MYSQL_URL || {
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
@@ -11,16 +11,7 @@ const pool = mysql.createPool(process.env.DATABASE_URL || {
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-}|| {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+}
 
 // Test connection
 (async () => {
@@ -33,4 +24,4 @@ const pool = mysql.createPool(process.env.DATABASE_URL || {
     }
 })();
 
-module.exports = pool;
+module.exports = pool.promise();
