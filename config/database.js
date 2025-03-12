4ca80172c676a5e3b3f8d8d03fa0918d7fa57e9c
Dineshwar-2501 <mahesg12war456@gmail.com>
2025-03-12 11:28:34 +0530

    URL

--- config/database.js
+++ config/database.js
@@ -3,6 +3,15 @@ require('dotenv').config(); // Load environment variables
 
 // Ensure DATABASE_URL is correctly set in Railway
 const pool = mysql.createPool(process.env.DATABASE_URL || {
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}|| {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
