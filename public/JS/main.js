2d7f08486be8a53702852d0d98ac2ddacc31f528
Dineshwar.S <123813656+Dineshwar-2501@users.noreply.github.com>
2025-03-24 15:41:14 +0530

    Update main.js

--- public/JS/main.js
+++ public/JS/main.js
@@ -16,7 +16,7 @@ window.addEventListener("load", async () => {
                 // Now register a fresh service worker
                 return navigator.serviceWorker.register("./service-worker.js");
             }).then((registration) => {
-                console.log("✅ Service Worker registered:", registration.scope);
+                // console.log("✅ Service Worker registered:", registration.scope);
             }).catch((error) => {
                 console.error("❌ Service Worker registration failed:", error);
             });
